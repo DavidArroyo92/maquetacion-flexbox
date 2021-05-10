330aac9e7ab8cab3ffe419ad06fb1ca54edc2811
DavidArroyo <david.arroyo@ecloudsolutions.com>
2021-05-10 17:11:36 -0300

    se agrega carousel y se soluciona problema con scroll x

--- js/script.js
+++ js/script.js
@@ -28,8 +28,22 @@ $(document).ready(function(){
         $(".side-bar").removeClass("active");
     });
 
-    $( function() {
-        $( "#dialog" ).dialog();
-      } );
+    //CAROUSEL
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
 
 });
